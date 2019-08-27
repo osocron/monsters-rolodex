24d2aa18dab29989495829bb2cb268c14714889a
osocron <osocron@gmail.com>
2019-08-27 16:22:06 -0500

    Updates

---
+++ precache-manifest.330f6357d94a8e3c377eb30eeb7dc841.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7498e2e7695452acbd1ed6c4b89870d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f06b5bbdc1b8ea671712",
+    "url": "/monsters-rolodex/static/css/main.2bd6ecab.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "f06b5bbdc1b8ea671712",
+    "url": "/monsters-rolodex/static/js/main.b528647b.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
